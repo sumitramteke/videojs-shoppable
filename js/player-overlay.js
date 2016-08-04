e316e7c29492a72ab9a6d24569b6c1f2050e2db1
sumitramteke <sumitvramteke@gmail.com>
2016-08-04 16:30:46 +0530

    make those button clickage events

--- js/player-overlay.js
+++ js/player-overlay.js
@@ -1,23 +1,46 @@
-(function(player, debug) {
+(function(player, debug, doc) {
+	let elms = [];
+	
+	let linkElm = doc.createElement('a');
+	linkElm.textContent = 'First Shoppable Item';
+	linkElm.setAttribute('href','http://www.google.co.in');
+	linkElm.setAttribute('class','btn btn-primary');
+	linkElm.setAttribute('target','_blank');
+	elms.push(linkElm);
+
+	linkElm = doc.createElement('a');
+	linkElm.textContent = 'Second Shoppable Item';
+	linkElm.setAttribute('href','http://www.google.co.in');
+	linkElm.setAttribute('class','btn btn-primary');
+	linkElm.setAttribute('target','_blank');
+	elms.push(linkElm);
+
+	linkElm = doc.createElement('a');
+	linkElm.textContent = 'Third Shoppable Item';
+	linkElm.setAttribute('href','http://www.google.co.in');
+	linkElm.setAttribute('class','btn btn-primary');
+	linkElm.setAttribute('target','_blank');
+	elms.push(linkElm);
+
 	player.overlay({
 		debug: debug,
 		showBackground:false,
-		class: 'btn btn-primary',
+		// class: 'btn btn-primary',
 		overlays: [{
-		  content: 'First Shoppable Item',
+		  content: elms[0],
 		  start: 0,
 		  end: 16,
 		  align: 'top-right'
 		}, {
-		  content: 'Second Shoppable Item',
+		  content: elms[1],
 		  start: 17,
 		  end: 27,
 		  align: 'right'
 		}, {
-		  content: 'Third Shoppable Item',
+		  content: elms[2],
 		  start: 28,
 		  end: 38,
 		  align: 'bottom-right'
 		}]
 	});
-}(GBL.player, GBL.debug));
+}(GBL.player, GBL.debug, window.document));
